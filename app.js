59dfaa7bdbd31ed792f24088358a5818557143c8
Tony8990 <tony.fava@edu.itspiemonte.it>
2017-11-08 10:08:19 +0100

    PRova

---
+++ app.js
@@ -0,0 +1,53 @@
+var fs =require('fs');
+var Log = require('log'),
+    log = new Log('debug');
+var ip =require('ip');
+var port = process.env.PORT || 8000;
+var options={
+    key: fs.readFileSync('credential/server.key'),
+    cert:fs.readFileSync('credential/server.crt'),
+    ca:fs.readFileSync('credential/server.csr')
+
+};
+var express = require("express"),
+    app =  express(),
+    server = require('https').createServer(options,app);
+var io = require("socket.io")(server);
+
+app.use(express.static(__dirname + "/public"));
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
+
+users = [];
+// Array with some colors
+var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
+// ... in random order
+colors.sort(function(a,b) { return Math.random() > 0.5; } );
+
+io.on('connection', function(socket) {
+    //console.log('A user connected');
+    socket.on('setUsername', function(data) {
+
+        console.log(data);
+
+        if(users.indexOf(data) > -1) {
+            socket.emit('userExists', ' Username ' + data + ' is taken! Try some other username.');
+        } else {
+            users.push(data);
+            socket.emit('userSet', {username: data, color: colors.shift()});
+        }
+    });
+
+    socket.on('msg', function(data,color) {
+        io.sockets.emit('newmsg', data,color);
+    });
+
+    socket.on('stream', function (image) {
+        socket.broadcast.emit('stream', image);
+    });
+});
+
+server.listen(port, function() {
+    log.info('listening on https://'+ip.address()+':'+port);
+});
